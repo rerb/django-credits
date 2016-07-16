9a0901fc85e7582ff83931fa9ea09e52bb97cdfc
rerb <bob.erb@aashe.org>
2016-07-16 05:34:46 -0400

    Change carousel delay with entry in settings.py

--- django_credits/templatetags/django_credits.py
+++ django_credits/templatetags/django_credits.py
@@ -1,10 +1,17 @@
 from django import template
+from django.conf import settings
 
 from ..models import Credit
 
+
 register = template.Library()
+
+DEFAULT_DJANGO_CREDITS_DELAY = 4000  # milliseconds
 
 
 @register.inclusion_tag("django_credits.html")
 def django_credits():
-    return {"credits": Credit.objects.all()}
+    return {"credits": Credit.objects.all(),
+            "django_credits_delay": getattr(settings,
+                                            "DJANGO_CREDITS_DELAY",
+                                            DEFAULT_DJANGO_CREDITS_DELAY)}
