0512c8db77206f1994ccd0c4e0b560f0b370c9c6
rerb <bob.erb@aashe.org>
2016-07-24 10:19:50 -0400

    Add support for multiple lightbox implementations.

--- django_credits/templatetags/django_credits.py
+++ django_credits/templatetags/django_credits.py
@@ -7,11 +7,17 @@ from ..models import Credit
 register = template.Library()
 
 DEFAULT_DJANGO_CREDITS_DELAY = 3000  # milliseconds
+DEFAULT_DJANGO_CREDITS_LIGHTBOX_VERSION = "bootstrap"
 
 
 @register.inclusion_tag("django_credits.html")
 def django_credits():
     return {"credits": Credit.objects.all(),
-            "django_credits_delay": getattr(settings,
-                                            "DJANGO_CREDITS_DELAY",
-                                            DEFAULT_DJANGO_CREDITS_DELAY)}
+            "django_credits_delay": getattr(
+                settings,
+                "DJANGO_CREDITS_DELAY",
+                DEFAULT_DJANGO_CREDITS_DELAY),
+            "lightbox_version": getattr(
+                settings,
+                "DJANGO_CREDITS_LIGHTBOX_VERSION",
+                DEFAULT_DJANGO_CREDITS_LIGHTBOX_VERSION)}
