49e0b0723e161cd72c883a6611831f8133696ee6
rerb <bob.erb@aashe.org>
2016-09-18 22:57:05 -0400

    Get rid of ordinal field.

--- django_credits/models.py
+++ django_credits/models.py
@@ -31,20 +31,12 @@ class Credit(models.Model):
     contributors = models.ManyToManyField(Contributor,
                                           through="Contribution",
                                           blank=True)
-    ordinal = models.PositiveIntegerField(blank=True,
-                                          db_index=True)
 
     class Meta:
-        ordering = ("ordinal",)
+        ordering = ("headline",)
 
     def __str__(self):
         return self.headline
-
-    def save(self, *args, **kwargs):
-        if not self.ordinal:
-            last_credit = Credit.objects.last()
-            self.ordinal = last_credit.ordinal + 10 if last_credit else 10
-        return super(Credit, self).save(*args, **kwargs)
 
 
 class Contribution(models.Model):
